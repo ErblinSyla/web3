053c004080b1269f8f24af32de52a48655c23cb4
Erblin Syla <erblin.sylaa@gmail.com>
2025-07-10 18:01:53 +0200

    Test 2"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,18 @@
 'use client';
 
-import { Inter } from 'next/font/google';
 import { ThirdwebProvider } from '@thirdweb-dev/react';
 import { ZMainnet } from '@thirdweb-dev/chains';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const inter = Inter({ subsets: ['latin'] });
-const queryClient = new QueryClient();
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <QueryClientProvider client={queryClient}>
-          <ThirdwebProvider activeChain={ZMainnet}>
-            {children}
-          </ThirdwebProvider>
-        </QueryClientProvider>
+      <body>
+        <ThirdwebProvider
+          activeChain={ZMainnet}
+          clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
+        >
+          {children}
+        </ThirdwebProvider>
       </body>
     </html>
   );
